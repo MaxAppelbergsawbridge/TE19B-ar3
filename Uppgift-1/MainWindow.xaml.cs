af03d2a6e0f380197435d3352cb54c95c87d6170
why u ask me now computer <mappelbridge@gmail.com>
2021-08-26 11:08:36 +0200

    WPF uppgifter 26/08

---
+++ Uppgift-1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void knapp (Object sender,RoutedEventArgs e)
+        {
+            string text= inputtext.Text;
+            int nyckel=0;
+            string nyckelString=inputtext.Text;
+            while (!int.TryParse(nyckelString,out nyckel))
+            {
+                OutputText.Text="Du måste mata in ett tal mellan (1-9)Försök igen";
+            }
+            string kryptadText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char bokstav = text[i];
+                Console.WriteLine($"Bokstaven på position {i} är {bokstav}");
+
+                int ascii = (int)bokstav;
+
+                int kryptadAscii = ascii + nyckel;
+
+                if (kryptadAscii >= 90)
+                {
+                    kryptadAscii -= 26;
+                }
+
+                char kryptadBokstav = (char)(kryptadAscii);
+                Console.WriteLine($"{bokstav} blir {kryptadBokstav}");
+
+                kryptadText += kryptadBokstav.ToString();
+            }
+
+            outputkrypterad.Text=($"Den krypterade texten blir: {kryptadText}");
+        }
+    }
+}
---
+++ WPF-labb/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_labb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textruta.Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        }
+        private void Skicka_Click(Object sender,RoutedEventArgs e)
+        {
+            MessageBox.Show(inputruta.Text);
+        }
+    }
+}
