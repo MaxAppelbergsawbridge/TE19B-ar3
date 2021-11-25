dead238521cf3b1d4afbefa2fdb8d78a7e4a0a8d
why u ask me now computer <mappelbridge@gmail.com>
2021-11-25 11:09:45 +0100

    Jobbat på Rest, API, avtolkning av kompliceratsvar

---
+++ nätverk/ChuckNorrisAPI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RestSharp;
+using System.Text.Json;
+namespace ChuckNorrisAPI
+{
+    class Joke
+    {
+        public string icon_url { get; set; }
+        public string value { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chuck Norris skämt");
+            string url = "https://api.chucknorris.io/jokes";
+            var client = new RestClient(url);
+            var request = new RestRequest("random", DataFormat.Json);
+            var response = client.Get(request);
+            Joke skämtet = new Joke();
+            skämtet = JsonSerializer.Deserialize<Joke>(response.Content);
+            System.Console.WriteLine(skämtet.value);
+        }
+    }
+}
---
+++ nätverk/Hämta pokemoninfo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System;
+using RestSharp;
+using System.Text.Json;
+namespace Hämta_pokemoninfo
+
+{
+    class Program
+    {
+        class Pokemon
+        {
+            public string name { get; set; }
+        }
+        class Pokemons
+        {
+            public int count { get; set; }
+            public List<Pokemon> results { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hämta alla pokemon!");
+            string url = " https://pokeapi.co/api/v2/";
+            var client = new RestClient(url);
+            var request = new RestRequest("pokemon", DataFormat.Json);
+            var response = client.Get(request);
+            //kolla på svaret
+            //System.Console.WriteLine(response.Content);
+            Pokemons Lista = new Pokemons();
+            Lista = JsonSerializer.Deserialize<Pokemons>(response.Content);
+            if (Lista != null)
+            {
+                foreach (var item in Lista.results)
+                {
+                    System.Console.WriteLine(item.name);
+                }
+            }
+        }
+    }
+}
