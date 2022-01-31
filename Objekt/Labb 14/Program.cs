097ad5d95bb83accff852bb63b7c1a04fc32b24a
why u ask me now computer <mappelbridge@gmail.com>
2022-01-31 12:52:58 +0100

    labb 14b med konstruktor

--- Objekt/Labb 14/Program.cs
+++ Objekt/Labb 14/Program.cs
@@ -6,12 +6,24 @@ namespace Labb_14
     {
         static void Main(string[] args)
         {
-            bok förslag = new bok();
-            förslag.LäsInTitel();
-            förslag.LäsInFöfattaren();
-            förslag.LäsInAntalSidor();
-            förslag.Skrivut();
+            Console.WriteLine("Vad heter boken?");
+            string titel = Console.ReadLine();
+            Console.WriteLine("Vem skrev boken?");
+            string författare = Console.ReadLine();
+            Console.WriteLine("Hur lång är boken?");
+            int antalsidor = ReadInt();
+            Bok bok = new Bok(titel, författare, antalsidor);
+            bok.Skrivut();
         }
-
+        static int ReadInt()
+        {
+            int heltal;
+            while (!int.TryParse(Console.ReadLine(), out heltal))
+            {
+                Console.WriteLine("Du skrev inte in ett heltal. Försök igen.");
+            }
+            return heltal;
+        }
     }
+
 }
--- Objekt/Labb 14/bok.cs
+++ Objekt/Labb 14/bok.cs
@@ -2,38 +2,21 @@ using System;
 
 namespace Labb_14
 {
-    public class bok
+    public class Bok
     {
         private string Titel { get; set; }
         private string Författare { get; set; }
         private int Antalsidor { get; set; }
         public void Skrivut()
         {
-            Console.WriteLine($"Boken är {Titel} av{Författare}, den är {Antalsidor} lång");
+            Console.WriteLine($"Boken är {Titel} av {Författare}, den är {Antalsidor} sidor lång");
         }
-        public void LäsInTitel()
+
+        public Bok(string _titel, string _författare, int _antalsidor)
         {
-            Console.WriteLine("Vad heter boken?");
-            Titel = Console.ReadLine();
-        }
-        public void LäsInFöfattaren()
-        {
-            Console.WriteLine("Vad heter författaren?");
-            Författare = Console.ReadLine();
-        }
-        public void LäsInAntalSidor()
-        {
-            Console.WriteLine("Hur lång är boken?");
-            Antalsidor = ReadInt();
-        }
-        static int ReadInt()
-        {
-            int heltal;
-            while (!int.TryParse(Console.ReadLine(), out heltal))
-            {
-                Console.WriteLine("Du skrev inte in ett heltal. Försök igen.");
-            }
-            return heltal;
+            Titel = _titel;
+            Författare = _författare;
+            Antalsidor = _antalsidor;
         }
     }
 }
